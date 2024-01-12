bf4da12644985ca48042d9e6ee50a5881830af04
lealepp <lea.lepp@tktk.ee>
2024-01-12 20:06:48 +0200

    add Week6StringsAndArrays code

---
+++ ArrayWithNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = new int[10];
+Random rnd = new Random();
+
+int sum = 0;
+
+//populating the array
+for (int i = 0; i < numbers.Length; i++)
+{ numbers[i] = rnd.Next(1, numbers.Length +1 );       //täidame masiivi
+    sum = sum + numbers[1];
+}                                                
+
+foreach( int number in numbers)                      //kuvame väärtused
+{ Console.WriteLine(number); }
+
+Console.WriteLine($"Total: {sum}");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿string hello = "we are the champions?";
+
+int stringLenght = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+int wordCounter = 1;
+int i = 0;
+{
+    if (char.IsWhiteSpace(hello[i]))
+    { wordCounter++; }
+    Console.WriteLine($"Word count in {hello}: {wordCounter + 3} words");}
